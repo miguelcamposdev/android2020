bb3f99c44dc0513ecfcc1c20e06edba4c07b5a10
Miguel Campos <camposmiguel@gmail.com>
2020-01-15 09:40:45 +0100

    ListView personalizado

--- 05_CustomListView/app/src/main/java/com/miguelcr/customlistview/AlumnosAdapter.java
+++ 05_CustomListView/app/src/main/java/com/miguelcr/customlistview/AlumnosAdapter.java
@@ -5,9 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -28,7 +32,26 @@ public class AlumnosAdapter extends ArrayAdapter<Alumno> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View v = LayoutInflater.from(ctx).inflate(layoutPlantilla, parent, false);
 
-        // ...
+        // Rescatar una referencia de cada elemento visual de la plantilla (template)
+        TextView tvNombre = v.findViewById(R.id.textViewNombre);
+        TextView tvEdad = v.findViewById(R.id.textViewEdad);
+        ImageView ivFoto = v.findViewById(R.id.imageViewFoto);
+
+        // Obtener los datos del alumno actual que debo dibujar
+        Alumno alumnoActual = listaDatos.get(position);
+        String nombre = alumnoActual.getNombre();
+        int edad = alumnoActual.getEdad();
+        String urlFoto = alumnoActual.getFotoUrl();
+
+        // Insertar en los componentes de la plantilla
+        // los datos del alumno actual
+        tvNombre.setText(nombre);
+        tvEdad.setText(String.valueOf(edad));
+
+        Glide.with(ctx)
+                .load(urlFoto)
+                .centerCrop()
+                .into(ivFoto);
 
         return v;
     }
--- 05_CustomListView/app/src/main/java/com/miguelcr/customlistview/MainActivity.java
+++ 05_CustomListView/app/src/main/java/com/miguelcr/customlistview/MainActivity.java
@@ -37,5 +37,6 @@ public class MainActivity extends AppCompatActivity {
         );
 
         // Conectar el adapter a la lista
+        lvAlumnos.setAdapter(adapter);
     }
 }
