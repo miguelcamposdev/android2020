3a483e18a72e26a351c46902fc40dfa71ee9e2f1
Miguel Campos <camposmiguel@gmail.com>
2020-02-05 14:16:43 +0100

    - Mapa en Activity

---
+++ 16_GoogleMapsFragment/app/src/main/java/com/miguelcr/a16_googlemapsfragment/MapsActivity.java
@@ -0,0 +1,64 @@
+package com.miguelcr.a16_googlemapsfragment;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // For
+        Marker m = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(-34, 151))
+                .title("Capital")
+                .snippet("Nº habitantes, idioma")
+        );
+        // Tendríais que cargar este String: pais.getIsoCode();
+        m.setTag("iso-code");
+
+        // end for
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String isoCode = marker.getTag().toString();
+                return false;
+            }
+        });
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+    }
+}
