6a43bc42befedf15141d4fad62ee7394619be493
Miguel Campos <camposmiguel@gmail.com>
2020-02-06 10:21:06 +0100

    Ahí vaaaa

---
+++ 16_GoogleMapsFragment/app/src/main/java/com/miguelcr/a16_googlemapsfragment/MapaEnFragment.java
@@ -0,0 +1,45 @@
+package com.miguelcr.a16_googlemapsfragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapaEnFragment extends Fragment implements OnMapReadyCallback {
+
+
+    public MapaEnFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_mapa_en, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
